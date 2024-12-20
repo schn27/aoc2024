1b9d1c8c63b64e8a7ef45d8c887d8aea6a1189e6
Aleksandr Malikov <schn27@gmail.com>
2024-12-20 22:54:57 +0300

    refactor day 15

--- 15.py
+++ 15.py
@@ -5,79 +5,68 @@ import re
 DIRS = {'^': (0, -1), '>': (1, 0), 'v': (0, 1), '<': (-1, 0)}
 
 warehouse, moves = data.split('\n\n')
+warehouse = list(map(list, warehouse.split('\n')))
 moves = list(''.join(moves.split('\n')))
 
-walls = set()
-boxes = set()
-robot = None
+def get_moving(warehouse, x, y, dx, dy):
+    c = warehouse[y][x]
 
-walls2 = set()
-boxes2 = set()
-robot2 = None
-
-for y, row in enumerate(warehouse.split('\n')):
-    for x, c in enumerate(row):
-        if c == '#':
-            walls.add((x, y))
-            walls2.add((x * 2, y))
-            walls2.add((x * 2 + 1, y))
-        elif c == 'O':
-            boxes.add((x, y))
-            boxes2.add((x * 2, y))
-        elif c == '@':
-            robot = (x, y)
-            robot2 = (x * 2, y)
-
-def get_moving_boxes_a(walls, boxes, xy, dxdy):
-    if xy in walls:
+    if c == '#':
         return None
-    if xy not in boxes:
+    if c == '.':
         return []
-    moving = get_moving_boxes_a(walls, boxes, (xy[0] + dxdy[0], xy[1] + dxdy[1]), dxdy)
-    return None if moving is None else [xy] + moving
+    if c == 'O' or dx != 0:
+        moving = get_moving(warehouse, x + dx, y + dy, dx, dy)
+        return None if moving is None else [(x, y, c)] + moving
 
-def get_moving_boxes_b(walls, boxes, xy, dxdy):
-    if xy in walls:
-        return None
+    side = 1 if c == '[' else -1
+    c2 = warehouse[y][x + side]
 
-    if dxdy[0] != 0:
-        xy = xy if dxdy[0] > 0 else (xy[0] - 1, xy[1])
-        if xy not in boxes:
-            return []
-        moving = get_moving_boxes_b(walls, boxes, (xy[0] + (-1 if dxdy[0] == -1 else 2), xy[1]), dxdy)
-        return None if moving is None else [xy] + moving
-    else:
-        xy2 = (xy[0] - 1, xy[1])
-        if xy not in boxes and xy2 not in boxes:
-            return []
-        if xy not in boxes:
-            xy = xy2
+    moving1 = get_moving(warehouse, x + dx, y + dy, dx, dy)
+    moving2 = get_moving(warehouse, x + dx + side, y + dy, dx, dy)
+    return None if moving1 is None or moving2 is None else [(x, y, c), (x + side, y, c2)] + moving1 + moving2
 
-        moving1 = get_moving_boxes_b(walls, boxes, (xy[0], xy[1] + dxdy[1]), dxdy)
-        moving2 = get_moving_boxes_b(walls, boxes, (xy[0] + 1, xy[1] + dxdy[1]), dxdy)
-        return None if moving1 is None or moving2 is None else [xy] + moving1 + moving2
-
-def do_moves(moves, robot, walls, boxes, part='a'):
-    boxes = boxes.copy()
-
-    get_moving_boxes = get_moving_boxes_a if part == 'a' else get_moving_boxes_b
+def do_moves(warehouse, robot):
+    x, y = robot
 
     for m in moves:
-        dxdy = DIRS[m]
-        new_robot = (robot[0] + dxdy[0], robot[1] + dxdy[1])
-        moving = get_moving_boxes(walls, boxes, new_robot, dxdy)
-        if moving is not None:
-            robot = new_robot
-            moving = set(moving)
-            for b in moving:
-                boxes.remove(b)
-            for b in moving:
-                boxes.add((b[0] + dxdy[0], b[1] + dxdy[1]))
+        dx, dy = DIRS[m]
+        x1, y1 = x + dx, y + dy
+        moving = get_moving(warehouse, x1, y1, dx, dy)
+        if moving is None:
+            continue
+        x, y = x1, y1
+        for bx, by, c in moving:
+            warehouse[by][bx] = '.'
+        for bx, by, c in moving:
+            warehouse[by + dy][bx + dx] = c
 
-    return boxes
+    return warehouse
 
-def get_gps_sum(boxes):
-    return sum(map(lambda e: e[0] + 100 * e[1], boxes))
+def get_gps_sum(warehouse):
+    res = 0
 
-submit(get_gps_sum(do_moves(moves, robot, walls, boxes)), part='a')
-submit(get_gps_sum(do_moves(moves, robot2, walls2, boxes2, 'b')), part='b')
+    for y, row in enumerate(warehouse):
+        for x, c in enumerate(row):
+            if c == 'O' or c == '[':
+                res += x + 100 * y
+    return res
+
+warehouse2 = []
+
+for y, row in enumerate(warehouse):
+    row2 = []
+    for x, c in enumerate(row):
+        if c in ['#', '.']:
+            row2 += [c] * 2
+        elif c == 'O':
+            row2 += ['[', ']']
+        elif c == '@':
+            warehouse[y][x] = '.'
+            robot = (x, y)
+            row2 += ['.', '.']
+            robot2 = (x * 2, y)
+    warehouse2.append(row2)
+
+submit(get_gps_sum(do_moves(warehouse, robot)), part='a')
+submit(get_gps_sum(do_moves(warehouse2, robot2)), part='b')
